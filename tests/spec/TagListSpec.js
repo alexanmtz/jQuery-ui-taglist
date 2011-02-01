f79a5217157310e15d73d180a4bef39621f463ad
Alexandre Magno Teles Zimerer <alexmagno@corp.globo.com>
2011-02-01 17:09:43 -0200

    first test passed! To check instance o jQuery ui

---
+++ tests/spec/TagListSpec.js
@@ -0,0 +1,12 @@
+describe("taglist", function() {
+
+  beforeEach(function() {
+    
+  });
+
+  it("should exist the jQuery ui instance", function() {
+    expect($.ui.taglist).toBeTruthy();
+  });
+
+  
+});
